160a420c761e27d5d10aa2e1893b5cb4d6096ecd
Adriza Mishra <41475637+azirdamishra@users.noreply.github.com>
2019-06-15 12:39:53 +0530

    Update and rename app.js to usingProcessObject.js

--- usingProcessObject.js
+++ usingProcessObject.js
@@ -2,15 +2,12 @@ console.log('Starting app.js');
 
 const fs = require('fs');
 const _ = require('lodash');
-const yargs = require('yargs');
 
 const notes = require('./notes.js');
 
-const argv = yargs.argv;
 var command = process.argv[2];
 console.log('Command: ', command);
 console.log('Process: ', process.argv);
-console.log('Yargs', argv);
 
 if(command === 'list'){
     console.log('listing all notes');
